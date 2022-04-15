a6d54d7255a978f12596564edbaa1a3d01f4fb51
Shiva Prasad <shivapv@pm.me>
2022-04-15 20:15:10 +0530

    Make a stripped-down version to work in Safari
    Safari does not support webRequestBlocking or responseHeaders. Hence we
    can neither check response MIME type nor redirect requests.

--- src/scripts/background.js
+++ src/scripts/background.js
@@ -2,19 +2,10 @@
  * Intercept loading of PDF files and redirect them to the add-on
  * Adapted from => [pdf.js]/extensions/chromium/pdfHandler.js
  */
-const pdfSources = {
-  urls: ["<all_urls>"],
-  types: ["main_frame", "sub_frame", "object"]
+const pdfUrl = {
+  url: [ {pathSuffix: ".pdf"}, {pathSuffix: ".PDF"} ]
 };
-const options = ["blocking", "responseHeaders"];
-const localPdf = {
-  url: [
-    {urlPrefix: "file://", pathSuffix: ".pdf"},
-    {urlPrefix: "file://", pathSuffix: ".PDF"}
-  ]
-};
-browser.webRequest.onHeadersReceived.addListener(redirect, pdfSources, options);
-browser.webNavigation.onBeforeNavigate.addListener(loadViewer, localPdf);
+browser.webNavigation.onBeforeNavigate.addListener(loadViewer, pdfUrl);
 browser.browserAction.onClicked.addListener(newViewer);
 
 /* Event handlers */
@@ -23,13 +14,14 @@ const getViewerURL = (url) => `${baseUrl}?file=${encodeURIComponent(url)}`;
 const messageUrl = getViewerURL("/pages/Retry.pdf");
 const splashUrl = getViewerURL("/pages/Open.pdf");
 
-function redirect(details) {
-  if (isPdfResp(details))
-    return { redirectUrl: getViewerURL(details.url) };
-}
 function loadViewer(details) {
-  if (!details.frameId)
-    browser.tabs.update(details.tabId, { url: messageUrl });
+  const {url, tabId, frameId} = details;
+  if (!frameId) {
+    if (url.startsWith("file://"))
+      browser.tabs.update(tabId, { url: messageUrl });
+    else
+      browser.tabs.update(tabId, { url: getViewerURL(url) });
+  }
 }
 function newViewer(_, clickData) {
   const btn = clickData?.button;
@@ -38,26 +30,4 @@ function newViewer(_, clickData) {
     browser.windows.create({ type: "popup", url: splashUrl });
   else
     browser.tabs.create({ url: splashUrl });
-}
-
-/* Determine if response is a PDF by inspecting its MIME type, file extension
- * or Content-Disposition header */
-function isPdfResp(details) {
-  const mimeType = getHeader(details.responseHeaders, "content-type");
-  if (mimeType) {
-    switch (mimeType.split(";", 1)[0].trim()) {
-      case "application/pdf":
-        return true;
-      case "application/octet-stream":
-        if (details.url.toLowerCase().indexOf(".pdf") > 0)
-          return true;
-        const cd = getHeader(details.responseHeaders, "content-disposition");
-        return /\.pdf(['"]|$)/i.test(cd);
-    }
-  }
-  return false;
-}
-function getHeader(respHeaders, header) {
-  const match = respHeaders.find(h => h.name.toLowerCase() === header);
-  return match && match.value.toLowerCase();
 }
