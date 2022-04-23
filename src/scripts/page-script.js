9b91ebe7a45406bbee816edd739fbbbcf892aa67
Shiva Prasad <shivapv@pm.me>
2022-04-23 14:03:26 +0530

    Show favicon from actual origin if avialable
    Helpful in organising multiple tabs while reading. This mimicks the
    behaviour of the built-in viewer, with doqment logo kept as fallback.

--- src/scripts/page-script.js
+++ src/scripts/page-script.js
@@ -1,10 +1,26 @@
 /* Rebranding */
 document.title = "doqment PDF Reader";
-linkIcon("/images/favicon.png");
+const favIcon = linkIcon("/images/favicon.png");
+
+/* Display host website favicon (if available) */
+const pdfUrl = getPdfUrl();
+if (self === top && pdfUrl?.startsWith("http")) {
+  const origIcon = new URL(pdfUrl).origin + "/favicon.ico";
+  fetch(origIcon, {method: "HEAD"}).then(resp => {
+    if (resp.ok && resp.headers.get("content-type")?.includes("image/"))
+      favIcon.href = origIcon;
+  });
+}
 
 function linkIcon(href) {
   const link = document.createElement("link");
   link.rel = "icon";
   link.href = href;
-  document.head.appendChild(link);
+  return document.head.appendChild(link);
+}
+
+/* Get original URL to PDF from query string */
+function getPdfUrl() {
+  const query = window.location.search.substring(1);
+  return new URLSearchParams(query).get("file");
 }
