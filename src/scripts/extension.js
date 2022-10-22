9a6475a7ccdc0ac5f11adcfcf19c428f88fcc432
Shiva Prasad <shivapv@pm.me>
2022-10-22 16:35:08 +0530

    Gh-pages for live demo viewer

--- src/scripts/extension.js
+++ src/scripts/extension.js
@@ -1,8 +1,4 @@
 /* Load extension scripts and add-on into the viewer page */
-if (window.location.protocol === "moz-extension:")
-  import("./page-script.js");
-else
-  import("./mv3-script.js");
-
+import "./mv3-script.js";
 import "./doqment.js";
-import "/doq/addon/doq.js";
+import "../doq/addon/doq.js";
--- src/scripts/mv3-script.js
+++ src/scripts/mv3-script.js
@@ -1,24 +1,8 @@
-/* Copy PDF URL to clipboard */
-chrome.runtime.onMessage.addListener(respond);
-
-function respond(request) {
-  if (request.action === "copyPdfURL") {
-    const pdfUrl = getPdfUrl();
-    if (pdfUrl.startsWith("http"))
-      navigator.clipboard.writeText(pdfUrl);
-  }
-}
-
-/* Get original URL to PDF from query string */
-function getPdfUrl() {
-  const query = window.location.search.substring(1);
-  return new URLSearchParams(query).get("file");
-}
-
 /* Make Solarized the pre-selected color scheme */
 if (!localStorage.getItem("doqment.init")) {
-  const pref = JSON.stringify({ scheme: 3 });
+  let pref = JSON.stringify({ scheme: 3, tone: "1" });
   localStorage.setItem("doq.preferences.light", pref);
+  pref = JSON.stringify({ scheme: 3, tone: "2" });
   localStorage.setItem("doq.preferences.dark", pref);
   localStorage.setItem("doqment.init", "true");
 }
