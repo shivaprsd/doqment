e468399dce9663368ac1e64b034c27b6026ed63d
Shiva Prasad <shivapv@pm.me>
2022-10-22 16:35:08 +0530

    Gh-pages for live demo viewer

--- src/scripts/extension.js
+++ src/scripts/extension.js
@@ -1,27 +1,15 @@
-import { chooseViewerPath, execOnEvent } from "./utils.js";
-
-/* Redirect to the latest PDF.js viewer if required */
-const defaultPath = "/pdfjs/";
-const viewerPath = await chooseViewerPath(defaultPath, "/pdfjs-latest/");
-
-if (!location.pathname.startsWith(viewerPath)) {
-  location.replace(location.href.replace(defaultPath, viewerPath));
-}
+import { execOnEvent } from "./utils.js";
 
 /* Load extension scripts and add-on into the viewer page */
-if (location.protocol === "moz-extension:") {
-  import("./mv2/page-script.js");
-} else {
-  import("./mv3/page-script.js");
-}
+import "./mv2/page-script.js";
 import "./doqment.js";
-import "/doq/addon/doq.js";
+import "../doq/addon/doq.js";
 
 /* TEMP: Enable signature editor and comment annotations by default */
 function enableSignAndComment() {
   const app = window.PDFViewerApplication;
   app.preferences.set("enableSignatureEditor", true);
   app.preferences.set("enableComment", true);
-  localStorage.removeItem("doqment.update-1.0");
+  localStorage.removeItem("update-1.0");
 }
 execOnEvent("update-1.2", [enableSignAndComment])
--- src/scripts/mv2/page-script.js
+++ src/scripts/mv2/page-script.js
@@ -1,43 +1,24 @@
-import { getPdfUrl, addLink, execOnEvent, isTouchScreen } from "../utils.js";
+import { addLink, isTouchScreen, execOnEvent } from "../utils.js";
 
 /* Rebranding */
 document.title = "doqment PDF Reader";
-const favIcon = addLink("icon", "/images/icon32.png");
+const favIcon = addLink("icon", "../../images/icon32.png");
 
-/* Display host website favicon (if available) */
-const pdfUrl = getPdfUrl();
-if (self === top && pdfUrl?.startsWith("http")) {
-  const origIcon = new URL(pdfUrl).origin + "/favicon.ico";
-  fetch(origIcon, {method: "HEAD"}).then(resp => {
-    if (resp.ok && resp.headers.get("content-type")?.includes("image/"))
-      favIcon.href = origIcon;
-  });
-  browser.tabs.getCurrent().then(tab => {
-    browser.pageAction.show(tab.id);
-  });
-}
-
-/* Send the current page's clean URL to the pageAction popup */
-browser.runtime.onMessage.addListener((request, _, sendResponsce) => {
-  if (request.action === "getURL")
-    sendResponsce({ url: window.PDFViewerApplication.baseUrl });
-});
-
-/* Make Firefox the pre-selected color scheme */
-function selectFirefox() {
-  /* scheme = 0 because of the migration code in doq */
-  const pref = JSON.stringify({ scheme: 0 });
+/* Make Solarized the pre-selected color scheme */
+function selectSolarized() {
+  /* scheme = 3 because of the migration code in doq */
+  let pref = JSON.stringify({ scheme: 3, tone: "1" });
   localStorage.setItem("doq.preferences.light", pref);
+  pref = JSON.stringify({ scheme: 3, tone: "2" });
   localStorage.setItem("doq.preferences.dark", pref);
 }
 
 /* Disable the annotation editors by default on mobile */
 async function disableAnnotEditors() {
-  const platform = await browser.runtime.getPlatformInfo();
-  if (platform.os === "android" && isTouchScreen()) {
+  if (isTouchScreen()) {
     const app = window.PDFViewerApplication;
     app.preferences.set("annotationEditorMode", -1);
   }
 }
 
-execOnEvent("init", [selectFirefox, disableAnnotEditors])
+execOnEvent("init", [selectSolarized, disableAnnotEditors]);
