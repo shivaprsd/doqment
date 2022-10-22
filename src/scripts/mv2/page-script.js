bf70182c967822455de737a653ca9893a3b0a110
Shiva Prasad <shivapv@pm.me>
2022-10-22 16:35:08 +0530

    Gh-pages for live demo viewer

--- src/scripts/mv2/page-script.js
+++ src/scripts/mv2/page-script.js
@@ -1,37 +1,16 @@
-import { getPdfUrl, addLink, execOnEvent, isTouchScreen } from "../utils.js";
+import { addLink, execOnEvent } from "../utils.js";
 
 /* Rebranding */
 document.title = "doqment PDF Reader";
-const favIcon = addLink("icon", "/images/icon32.png");
+const favIcon = addLink("icon", "../../images/icon32.png");
 
-/* Display host website favicon (if available) */
-const pdfUrl = getPdfUrl();
-if (self === top && pdfUrl?.startsWith("http")) {
-  const origIcon = new URL(pdfUrl).origin + "/favicon.ico";
-  fetch(origIcon, {method: "HEAD"}).then(resp => {
-    if (resp.ok && resp.headers.get("content-type")?.includes("image/"))
-      favIcon.href = origIcon;
-  });
-  browser.tabs.getCurrent().then(tab => {
-    browser.pageAction.show(tab.id);
-  });
-}
-
-/* Make Firefox the pre-selected color scheme */
-function selectFirefox() {
-  /* scheme = 0 because of the migration code in doq */
-  const pref = JSON.stringify({ scheme: 0 });
+/* Make Solarized the pre-selected color scheme */
+function selectSolarized() {
+  /* scheme = 3 because of the migration code in doq */
+  let pref = JSON.stringify({ scheme: 3, tone: "1" });
   localStorage.setItem("doq.preferences.light", pref);
+  pref = JSON.stringify({ scheme: 3, tone: "2" });
   localStorage.setItem("doq.preferences.dark", pref);
-}
-
-/* Disable the annotation editors by default on mobile */
-async function disableAnnotEditors() {
-  const platform = await browser.runtime.getPlatformInfo();
-  if (platform.os === "android" && isTouchScreen()) {
-    const app = window.PDFViewerApplication;
-    app.preferences.set("annotationEditorMode", -1);
-  }
 }
 
 /* Disable hardware accelerated rendering of canvas */
@@ -40,5 +19,4 @@ function forceSoftwareRender() {
   localStorage.setItem("doq.options", JSON.stringify(doqOptions));
 }
 
-execOnEvent("init", [selectFirefox, disableAnnotEditors])
-execOnEvent("update-0.9", [forceSoftwareRender])
+execOnEvent("init", [selectSolarized, forceSoftwareRender])
