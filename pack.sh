0e3fd0a39476bdd6ff58399ffc6989b13f51f204
Shiva Prasad <shivapv@pm.me>
2022-12-22 14:10:30 +0530

    Add option to specify target manifest to pack

--- pack.sh
+++ pack.sh
@@ -1,6 +1,13 @@
 #!/bin/sh
 # Package the extension to a version-numbered Zip file.
 
+variant=${1:-mv2}
+if [ "$variant" != mv2 ] && [ "$variant" != mv3 ]; then
+	echo "Unknown target: $variant"
+	exit
+fi
+ln -srf "src/manifest-${variant#?}.json" src/manifest.json
+
 version=$(awk -F: '/"version"/ {print $2}' src/manifest.json | tr -d ' ,"')
 target=../dist/doqment-v$version.zip
 
