7b622faf44e5f7f00544294f481197e5f3310a5d
Shiva Prasad <shivapv@pm.me>
2024-01-06 16:33:42 +0530

    Options page: add button to restore defaults, undo
    Also polish some finer aspects of the design and implementation.

--- src/scripts/options.js
+++ src/scripts/options.js
@@ -5,33 +5,47 @@ if (typeof browser === "undefined") {
 const pdfjsSchema = browser.runtime.getURL("pdfjs/preferences_schema.json");
 const pdfjsOptions = document.getElementById("pdfjsOptions");
 const moreOptions = document.getElementById("moreOptions").content;
+const resetOptions = document.getElementById("resetOptions");
+const undoReset = document.getElementById("undoReset");
 
+pdfjsOptions.addEventListener("change", () => {
+  resetOptions.disabled = false;
+  undoReset.disabled = true;
+});
 pdfjsOptions.addEventListener("change", updatePdfjsPref);
 moreOptions.querySelector("legend").addEventListener("change", toggleMore);
+resetOptions.onclick = undoReset.onclick = restorePdfjsPrefs;
 
 fetch(pdfjsSchema).then(resp => resp.json()).then(schema => {
+  const themeOption = schema.properties["viewerCssTheme"];
+  const captionFix = " for the viewer toolbars and background.";
+  themeOption.title = "Viewer theme";
+  themeOption.description = themeOption.description.replace(".", captionFix);
+
   delete schema.properties["disableTelemetry"];
-  render(schema, pdfjsOptions, moreOptions, updatePdfjsOption);
+  render(schema, pdfjsOptions, moreOptions, updatePdfjsControl);
 });
 
-async function render(schema, options, moreOptions, updateOption) {
+async function render(schema, options, moreOptions, updateControl) {
   const {properties} = schema;
+  let hasMoreOptions = false;
 
   for (let key in properties) {
-    const {title, description, default: default_} = properties[key];
+    const {title, description} = properties[key];
 
     if (description?.startsWith("DEPRECATED")) {
       continue;
     }
     const option = renderOption(key, properties[key]);
-    await updateOption(option, key, default_);
+    await updateControl(option.querySelector("#" + key));
     if (title) {
       options.appendChild(option);
     } else {
       moreOptions.firstElementChild.appendChild(option);
+      hasMoreOptions = true;
     }
   }
-  if (moreOptions)
+  if (hasMoreOptions)
     options.appendChild(moreOptions);
 }
 
@@ -48,29 +62,31 @@ function renderOption(key, property) {
   option.querySelector(".header").appendChild(control);
 
   if (type === "boolean" && browser === chrome) {
-    option.querySelector("div").onclick = toggleOption;
+    option.querySelector("div").addEventListener("click", toggleOption);
   }
   return option;
 }
 
 function renderControl(key, property) {
-  const {type, enum: nums} = property;
+  const {type, enum: nums, default: default_} = property;
 
   if (type === "boolean") {
-    return renderToggle(key);
+    return renderToggle(key, default_);
   }
   const control = (nums ? renderSelect : renderInput)(key, property);
   control.id = key;
+  control.value = default_;
   control.dataset.type = type;
   control.classList.add("optionField");
   return control;
 }
 
-function renderToggle(key) {
+function renderToggle(key, toggled) {
   const toggle = document.getElementById("toggle").content.cloneNode(true);
+  const input = toggle.querySelector("input");
 
-  toggle.querySelector("input").id = key;
-  toggle.querySelector("label").htmlFor = key;
+  toggle.querySelector("label").htmlFor = input.id = key;
+  input.checked = toggled;
   return toggle;
 }
 
@@ -106,9 +122,17 @@ function renderSelect(key, property) {
 
 const viewer = document.querySelector("iframe");
 const viewerLoad = new Promise(resolve => viewer.onload = resolve);
+let savedPrefs = {}
 
-async function updatePdfjsPref(e) {
+async function getPdfjsApp() {
+  await viewerLoad;
+  return viewer.contentWindow.PDFViewerApplication;
+}
+
+function updatePdfjsPref(e) {
   const {target} = e;
+  const key = target.id;
+
   if (!target.checkValidity()) {
     return;
   }
@@ -116,37 +140,57 @@ async function updatePdfjsPref(e) {
   if (target.dataset?.type === "integer") {
     value = Number(value);
   }
-  await viewerLoad;
-  const app = viewer.contentWindow.PDFViewerApplication;
-  app.preferences.set(target.id, value);
+  getPdfjsApp().then(pdfjs => pdfjs.preferences.set(key, value));
+  savedPrefs[key] = value;
 }
 
-async function updatePdfjsOption(option, key, defaultValue) {
-  await viewerLoad;
-  const app = viewer.contentWindow.PDFViewerApplication;
-  const value = await app.preferences.get(key);
+async function updatePdfjsControl(control) {
+  const key = control.id;
+  const pdfjs = await getPdfjsApp();
+  const value = await pdfjs.preferences.get(key);
 
-  const control = option.querySelector("#" + key);
   const attr = (control.type === "checkbox") ? "checked" : "value";
-  control[attr] = value ?? defaultValue;
+  savedPrefs[key] = control[attr] = value;
+}
+
+let stashedPrefs = {};
+
+/* Restore either default or stashed preferences, as per the event target */
+async function restorePdfjsPrefs() {
+  const reset = this.id === "resetOptions";
+  const pdfjs = await getPdfjsApp();
+
+  if (reset) {
+    Object.assign(stashedPrefs, savedPrefs);
+    await pdfjs.preferences.reset();
+  }
+  for (let key in savedPrefs) {
+    if (!reset) {
+      await pdfjs.preferences.set(key, stashedPrefs[key]);
+    }
+    updatePdfjsControl(document.getElementById(key));
+  }
+  const other = reset ? undoReset : resetOptions;
+  this.disabled = true;
+  other.disabled = false;
+  other.focus();
 }
 
 function toggleOption(e) {
   const {target} = e;
-  const tag = target.tagName.toLowerCase();
+  const {tagName} = target;
 
-  if (target === this || tag === "input" || tag === "label") {
+  if (target === this || tagName === "INPUT" || tagName === "LABEL") {
     return;
   }
   const input = this.querySelector("input");
   input.checked = !input.checked;
-  updatePdfjsPref({ target: input });
+  input.dispatchEvent(new Event("change", { bubbles: true }));
 }
 
 function toggleMore(e) {
   const {target} = e;
   const moreOptions = target.closest(".more.options");
   moreOptions.disabled = !target.checked;
-  moreOptions.scrollIntoView(true);
   e.stopPropagation();
 }
