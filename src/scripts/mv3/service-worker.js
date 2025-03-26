shivaprsd/doqment

commit a2e79b456cdd8593b26ba22818ee6fe260e308bf

MV3: update document title with iframed viewer
Follow up to ff3f688. Also update PDF.js to v5.0.

src/scripts/mv3/service-worker.js
@@ -122,6 +122,10 @@ function respond(request, sender, sendResponse) {
   } else if (request.action === "removeViewer") {
     const func = () => document.getElementById("doqmentViewer").remove();
     chrome.scripting.executeScript({ target: {tabId}, func });
+  } else if (request.action === "updateTitle") {
+    const func = title => document.title = title;
+    const title = request.body;
+    chrome.scripting.executeScript({ target: { tabId }, func, args: [title] });
   }
 }
 
@@ -203,9 +207,10 @@ function loadViewer(viewerUrl, tabId) {
   const injectFrame = src => {
     const frame = document.createElement("iframe");
     frame.src = src;
-    frame.id = "doqmentViewer";
+    frame.name = frame.id = "doqmentViewer";
     frame.setAttribute("allow", "fullscreen");
     document.body.prepend(frame);
+    document.querySelector("embed[type='application/pdf']")?.remove();
   };
   chrome.scripting.executeScript({
     target: {tabId}, func: injectFrame, args: [viewerUrl]

src/scripts/mv3/page-script.js
@@ -20,3 +20,12 @@ async function activateAndPrompt(message, reply) {
   await chrome.tabs.update(tab.id, { active: true });
   reply([{ result: window.confirm(message) }]);
 }
+
+if (window.name === "doqmentViewer") {
+  const updateTitle = () => {
+    const request = { action: "updateTitle", body: document.title };
+    chrome.runtime.sendMessage(request);
+  };
+  const title = document.querySelector("title");
+  new MutationObserver(updateTitle).observe(title, { childList: true });
+}

commit 5fb4561503e33d7c6dd104d475d7e0b1b3e04222

MV3: defer removing embed to avoid iframe flicker
This fixes a small visual glitch introduced by a2e79b4.

src/scripts/mv3/service-worker.js
@@ -210,12 +210,16 @@ function loadViewer(viewerUrl, tabId) {
     frame.name = frame.id = "doqmentViewer";
     frame.setAttribute("allow", "fullscreen");
     document.body.prepend(frame);
+  };
+  const removeEmbed = () => {
     document.querySelector("embed[type='application/pdf']")?.remove();
   };
   chrome.scripting.executeScript({
     target: {tabId}, func: injectFrame, args: [viewerUrl]
   }).then(() => {
     chrome.scripting.insertCSS({ target: {tabId}, files: [viewerCSS] });
+  }).then(() => {
+    chrome.scripting.executeScript({ target: {tabId}, func: removeEmbed });
   });
 }
 