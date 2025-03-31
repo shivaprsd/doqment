shivaprsd/doqment

commit 90bc236e97004ae6ca04ede1e2951a5859a69c51

MV2: show message for local PDF files only once

src/scripts/mv2/background.js
@@ -11,13 +11,16 @@ const pdfSources = {
 const options = ["blocking", "responseHeaders"];
 const localPdf = {
   url: [
-    {urlPrefix: "file://", pathSuffix: ".pdf"},
-    {urlPrefix: "file://", pathSuffix: ".PDF"}
+    { urlPrefix: "file://", pathSuffix: ".pdf" },
+    { urlPrefix: "file://", pathSuffix: ".PDF" }
   ]
 };
 browser.webRequest.onHeadersReceived.addListener(redirect, pdfSources, options);
-browser.webNavigation.onBeforeNavigate.addListener(loadViewer, localPdf);
 browser.browserAction.onClicked.addListener(newViewer);
+browser.runtime.onInstalled.addListener(details => {
+  if (details.reason === "install")
+    browser.webNavigation.onBeforeNavigate.addListener(showMessage, localPdf);
+});
 
 /* Event handlers */
 const baseUrl = browser.runtime.getURL("pdfjs/web/viewer.html");
@@ -28,9 +31,11 @@ function redirect(details) {
   if (isPdfResp(details) && !shouldDownload(details))
     return { redirectUrl: getViewerURL(baseUrl, details.url) };
 }
-function loadViewer(details) {
-  if (!details.frameId)
-    browser.tabs.update(details.tabId, { url: messageUrl });
+function showMessage(details) {
+  if (!details.frameId) {
+    browser.tabs.create({ openerTabId: details.tabId, url: messageUrl });
+    browser.webNavigation.onBeforeNavigate.removeListener(showMessage);
+  }
 }
 function newViewer(_, clickData) {
   const btn = clickData?.button;

commit 5baa72c1bdfa37fbc873c2f06fcb9b84c1fd7288

MV2: make sure the local PDF message is shown once
This fixes a regression in 90bc236 that the message would never be shown
if the background page gets terminated before even a single local PDF is
opened. Also made the message a bit more nicer.

src/scripts/mv2/background.js
@@ -2,7 +2,7 @@
  * Intercept loading of PDF files and redirect them to the add-on
  * Adapted from => [pdf.js]/extensions/chromium/pdfHandler.js
  */
-import { getViewerURL } from "../utils.js";
+import { getViewerURL, execOnEvent } from "../utils.js";
 
 const pdfSources = {
   urls: ["<all_urls>"],
@@ -17,10 +17,7 @@ const localPdf = {
 };
 browser.webRequest.onHeadersReceived.addListener(redirect, pdfSources, options);
 browser.browserAction.onClicked.addListener(newViewer);
-browser.runtime.onInstalled.addListener(details => {
-  if (details.reason === "install")
-    browser.webNavigation.onBeforeNavigate.addListener(showMessage, localPdf);
-});
+browser.webNavigation.onBeforeNavigate.addListener(showMessage, localPdf);
 
 /* Event handlers */
 const baseUrl = browser.runtime.getURL("pdfjs/web/viewer.html");
@@ -33,7 +30,8 @@ function redirect(details) {
 }
 function showMessage(details) {
   if (!details.frameId) {
-    browser.tabs.create({ openerTabId: details.tabId, url: messageUrl });
+    const messageTab = { openerTabId: details.tabId, url: messageUrl };
+    execOnEvent("open-local-pdf", [() => browser.tabs.create(messageTab)]);
     browser.webNavigation.onBeforeNavigate.removeListener(showMessage);
   }
 }